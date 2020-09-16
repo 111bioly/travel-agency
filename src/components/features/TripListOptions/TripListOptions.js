9773040ef7ddcd79d9f5e1809e62057be841f072
Tomasz Szabla <111thomas1111@gmail.com>
2020-09-16 21:00:36 +0200

    fix module 17.5

--- src/components/features/TripListOptions/TripListOptions.js
+++ src/components/features/TripListOptions/TripListOptions.js
@@ -9,15 +9,20 @@ class TripListOptions extends React.Component {
     if(checked) {
       console.log('Adding tag', tag);
       // TODO - use action dispatcher from props
+      this.props.filters.tags.push(tag);
     } else {
       console.log('Removing tag', tag);
       // TODO - use action dispatcher from props
+      const index = this.props.filters.tags.indexOf(tag);
+      this.props.filters.tags.splice(index, 1);
     }
+    this.props.changeTags(this.props.filters.tags);
   }
 
   handleDuration(type, value){
     console.log('Changing duration', type, value);
     // TODO - use action dispatcher from props
+    this.props.changeDuration(type, value);
   }
 
   handleSearch(phrase){
@@ -73,6 +78,8 @@ TripListOptions.propTypes = {
   tags: PropTypes.object,
   filters: PropTypes.object,
   changeSearchPhrase: PropTypes.func,
+  changeTags: PropTypes.func,
+  changeDuration: PropTypes.func,
 };
 
 export default TripListOptions;
--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -11,11 +11,14 @@ const createActionName = name => `app/${reducerName}/${name}`;
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
 // TODO - add other action types
+export const CHANGE_TAGS = createActionName('CHANGE_TAGS');
+export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
 
 // action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
 // TODO - add other action creators
-
+export const changeTags = payload => ({ payload, type: CHANGE_TAGS});
+export const changeDuration = payload => ({ payload, type: CHANGE_DURATION });
 // reducer
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
@@ -23,6 +26,16 @@ export default function reducer(statePart = [], action = {}) {
       return {
         ...statePart,
         searchPhrase: action.payload,
+      };
+    case CHANGE_TAGS:
+      return {
+        ...statePart,
+        tags: action.payload,
+      };
+    case CHANGE_DURATION:
+      return {
+        ...statePart,
+        duration: {...statePart.duration, [action.payload.type]: action.payload.value},
       };
     // TODO - handle other action types
     default:
